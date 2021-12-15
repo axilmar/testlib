cbaa02763fb2d8cca2f51f088c32a563f89254a1
axilmar <axilmar@gmail.com>
2021-12-15 14:30:04 +0200

    Added support for reporting multiple errors.

--- include/testlib.hpp
+++ include/testlib.hpp
@@ -10,6 +10,7 @@
 #include <mutex>
 #include <deque>
 #include <utility>
+#include <iomanip>
 
 
 #ifdef _WIN32
@@ -19,14 +20,9 @@
 
 #define check(cond) {\
     if (!(cond)) {\
-        if (test_thread) {\
-            ++testlib::test_error_count;\
-            throw testlib::test_error(__FILE__, __LINE__, #cond);\
-        }\
-        else {\
-            std::lock_guard lock(test_error_queue_mutex);\
-            test_error_queue.emplace_back(__FILE__, __LINE__, #cond);\
-        }\
+        ++testlib::get_globals().test_error_count;\
+        std::lock_guard lock(testlib::get_globals().test_error_queue_mutex);\
+        testlib::get_globals().test_error_queue.emplace_back(__FILE__, __LINE__, #cond);\
     }\
 }
 
@@ -44,32 +40,34 @@
         catch (const ex&) {\
             break;\
         }\
-        ++testlib::test_error_count;\
-        throw testlib::test_error(__FILE__, __LINE__, #expr);\
+        ++testlib::get_globals().test_error_count;\
+        std::lock_guard lock(testlib::get_globals().test_error_queue_mutex);\
+        testlib::get_globals().test_error_queue.emplace_back(__FILE__, __LINE__, #expr);\
     } while (0);
+
+
+#define fail_test_with_exception(ex) {\
+        ++testlib::get_globals().test_error_count;\
+        std::lock_guard lock(testlib::get_globals().test_error_queue_mutex);\
+        testlib::get_globals().test_error_queue.emplace_back(__FILE__, __LINE__, std::string("\u001b[33;1mException:\u001b[0m\u001b[33m ") + ex.what());\
+    }
 
 
 namespace testlib {
 
 
-    class test_error : public std::runtime_error {
-    public:
-        test_error(const char* file, int line, const std::string& msg) : std::runtime_error(msg), m_file(file), m_line(line) {}
-
-        const char* file() const { return m_file; }
-        int line() const { return m_line; }
-
-    private:
-        const char* m_file;
-        int m_line;
+    struct globals {
+        size_t test_row_length = 80;
+        std::atomic<size_t> test_error_count = 0;
+        std::mutex test_error_queue_mutex;
+        std::deque<std::tuple<const char*, int, std::string>> test_error_queue;
     };
 
 
-    inline size_t test_row_length = 80;
-    inline std::atomic<size_t> test_error_count = 0;
-    inline thread_local bool test_thread = false;
-    inline std::mutex test_error_queue_mutex;
-    inline std::deque<std::tuple<const char*, int, std::string>> test_error_queue;
+    inline globals& get_globals() {
+        static globals g;
+        return g;
+    }
 
 
     inline void init(size_t test_row_length_param = 80) {
@@ -79,18 +77,18 @@ namespace testlib {
         GetConsoleMode(console_handle, &mode);
         SetConsoleMode(console_handle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
         #endif
-        test_row_length_param = test_row_length;
-        test_error_count = 0;
+        test_row_length_param = get_globals().test_row_length;
+        get_globals().test_error_count = 0;
     }
 
 
     inline void cleanup() {
         std::cout << '\n';
-        if (test_error_count == 0) {
+        if (get_globals().test_error_count == 0) {
             std::cout << "No errors found.\n";
         }
         else {
-            std::cout << "Found " << test_error_count << (test_error_count > 1 ? " errors.\n" : " error.\n");
+            std::cout << "Found " << get_globals().test_error_count << (get_globals().test_error_count > 1 ? " errors.\n" : " error.\n");
         }
         std::cout << '\n';
     }
@@ -104,9 +102,8 @@ namespace testlib {
 
     //execute test
     template <class F> void test(const char* name, F&& proc) {
-        test_thread = true;
         static const auto dots = [](const std::string& s) {
-            const size_t count = s.size() < test_row_length ? test_row_length - s.size() : 5;
+            const size_t count = s.size() < get_globals().test_row_length ? get_globals().test_row_length - s.size() : 5;
             return " \u001b[36m" + std::string(count - 2, '.') + "\u001b[0m ";
         };
 
@@ -116,29 +113,33 @@ namespace testlib {
         const auto start = std::chrono::high_resolution_clock::now();
 
         try {
-            {
-                std::lock_guard lock(test_error_queue_mutex);
-                test_error_queue.clear();
-            }
             proc();
-            {
-                std::lock_guard lock(test_error_queue_mutex);
-                if (!test_error_queue.empty()) {
-                    const auto& first_error = test_error_queue.front();
-                    throw test_error(std::get<0>(first_error), std::get<1>(first_error), std::get<2>(first_error));
-                }
-            }
         }
-        catch (const test_error& ex) {
+        catch (const std::exception& ex) {
             const auto end = std::chrono::high_resolution_clock::now();
-            std::cout << dots(title) << "\u001b[31;1mERROR\u001b[0m" << duration_string(start, end) << std::endl;
-            std::cout << "    " << "\u001b[33m" << "File " << ex.file() << ", line " << ex.line() << ":" << "\u001b[0m" << std::endl;
+            std::cout << dots(title) << "\u001b[31;1mEXCEPTION\u001b[0m" << duration_string(start, end) << std::endl;
             std::cout << "    " << "\u001b[33m" << ex.what() << "\u001b[0m" << std::endl;
             return;
         }
 
         const auto end = std::chrono::high_resolution_clock::now();
-        std::cout << dots(title) << "\u001b[32;1mOK\u001b[0m" << duration_string(start, end) << std::endl;
+
+        std::lock_guard lock(get_globals().test_error_queue_mutex);
+
+        if (get_globals().test_error_queue.empty()) {
+            std::cout << dots(title) << "\u001b[32;1mOK\u001b[0m" << duration_string(start, end) << std::endl;
+        }
+
+        else {
+            std::cout << dots(title) << "\u001b[31;1m" << (get_globals().test_error_queue.size() > 1 ? "ERRORS" : "ERROR") << "\u001b[0m" << duration_string(start, end) << std::endl;
+            size_t index = 0;
+            for (const auto& error : get_globals().test_error_queue) {
+                std::cout << std::setw(3) << index << ") \u001b[33m" << "File " << std::get<0>(error) << ", line " << std::get<1>(error) << ":" << "\u001b[0m" << std::endl;
+                std::cout << "     " << "\u001b[33m" << std::get<2>(error) << "\u001b[0m" << std::endl;
+                ++index;
+            }
+            get_globals().test_error_queue.clear();
+        }
     }
 
 
